77a95c820f73dae7afe335835bb3872817cbcd3a
cuitianze <cuitianze123@gmail.com>
2016-06-04 21:04:18 +0800

    add jpush client

--- client/android/app/src/main/java/com/meimeiapp/MainActivity.java
+++ client/android/app/src/main/java/com/meimeiapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.meimeiapp;
 
 import com.facebook.react.ReactActivity;
+import cn.jpush.reactnativejpush.JPushPackage;
+import cn.reactnative.modules.jpush.JPushPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
@@ -34,7 +36,9 @@ public class MainActivity extends ReactActivity {
     @Override
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
-            new MainReactPackage()
+            new MainReactPackage(),
+            new JPushPackage(),
+            new JPushPackage()
         );
     }
 }
