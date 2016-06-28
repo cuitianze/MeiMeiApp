5cb638a581e5f1fa12ba835a06ef0c8a3f7701a0
cuitianze <cuitianze123@gmail.com>
2016-06-28 23:56:33 +0800

    add eact-native-sound

--- client/android/app/src/main/java/com/meimeiapp/MainActivity.java
+++ client/android/app/src/main/java/com/meimeiapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.meimeiapp;
 
 import com.facebook.react.ReactActivity;
+import com.zmxv.RNSound.RNSoundPackage;
 import cn.jpush.reactnativejpush.JPushPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -36,6 +37,7 @@ public class MainActivity extends ReactActivity {
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
+            new RNSoundPackage(),
             new JPushPackage()
         );
     }
